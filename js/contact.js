d8bcb7875443dcf29320f0f35235c419e5e100ee
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-22 03:09:29 +0100

    Add files

---
+++ js/contact.js
@@ -0,0 +1,40 @@
+var Contact = React.createClass({
+	propTypes: {
+		item: React.PropTypes.object.isRequired,
+  	},
+
+  	render: function() {
+		return (
+	  		<div className={'contactItem'}>
+    			<img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+    			<p className={'contactLabel'}>
+    				Imię: {this.props.contact.firstName}
+    			</p>
+                <p className={'contactLabel'}>
+                	Nazwisko: {this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto:' + this.props.item.email}> 
+                	{this.props.item.email} 
+                </a>
+			</div>
+	  	)
+  	}
+});
+
+
+/*
+return (
+	  		<div className={'contactItem'}>
+    			<img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+    			<p className={'contactLabel'}>
+    				{'Imię: ' + this.props.item.firstName}
+    			</p>
+                <p className={'contactLabel'}>
+                	{'Nazwisko: ' + this.props.item.lastName}
+                </p>
+                <a className={'contactEmail'} href={'mailto:' + this.props.item.email}> 
+                	{this.props.item.email} 
+                </a>
+			</div>
+*/
+
