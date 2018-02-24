b5d47ced6b54cd14def5b79c180cea6f26deb597
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-25 00:31:50 +0100

    Update files

--- js/contact.js
+++ js/contact.js
@@ -6,7 +6,7 @@ var Contact = React.createClass({
   	render: function() {
 		return (
 	  		<div className={'contactItem'}>
-    			<img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+    			<img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'} />
     			<p className={'contactLabel'}>
     				Imię: {this.props.item.firstName}
     			</p>
@@ -21,11 +21,10 @@ var Contact = React.createClass({
   	}
 });
 
-
 /*
-return (
+        return (
 	  		<div className={'contactItem'}>
-    			<img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+    			<img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'} />
     			<p className={'contactLabel'}>
     				{'Imię: ' + this.props.item.firstName}
     			</p>
